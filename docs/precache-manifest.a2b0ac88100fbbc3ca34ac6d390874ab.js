326601f12bb9cd8e586c55d1e5a1fd3b4dd68b80
Franciscoggonzalvez <f.garciagonzalvez@gmail.com>
2020-09-04 16:58:46 +0200

    creando docs

---
+++ docs/precache-manifest.a2b0ac88100fbbc3ca34ac6d390874ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989aa82e3674d548ca172c37ad2db760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d23cc611765623d61b05",
+    "url": "/static/css/main.8dbb453f.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d23cc611765623d61b05",
+    "url": "/static/js/main.485057f3.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
